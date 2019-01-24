fcc783c3c29c8c1cb96e750a1a6bdd5c7173c496
JP-Amboage <juangamboage@gmail.com>
2019-01-24 10:46:35 +0100

    volvi a poner avatar | imagen salir y salir carcel

--- interfazgrafica/LoginPanel.java
+++ interfazgrafica/LoginPanel.java
@@ -1,8 +1,6 @@
 package interfazgrafica;
 
-import java.awt.Component;
-import java.awt.Dimension;
-import java.awt.Image;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -25,6 +23,7 @@ public class LoginPanel extends JFrame{
 	private InterfazGrafica interfaz;
 	private JLabel fotoETSE;
 	private ArrayList<JTextField> fields = new ArrayList<JTextField>();
+	private ArrayList<Choice> choices = new ArrayList<>();
 	private JLabel n1_label;
 	private JLabel n2_label;
 	private JLabel n3_label;
@@ -33,9 +32,10 @@ public class LoginPanel extends JFrame{
 	private JLabel n6_label;
 	private JLabel introduzca;
 	private ButtonGroup grupo;
+	private static ArrayList<String> avatares= new ArrayList<>();
 	private ArrayList<JRadioButton> botones = new ArrayList<JRadioButton>();
 	private JButton comenzar;
-	
+
 	private static ArrayList<String> nombres = new ArrayList<String>();
 	private Casilla salida;
 
@@ -46,7 +46,7 @@ public class LoginPanel extends JFrame{
 		this.salida = salida;
 		this.setUp();
 	}
-	
+
 	private void setUp() throws IOException {
 		Image img = ImageIO.read(getClass().getResource("/resources/etse.jpg"));
 		img = img.getScaledInstance(40, 50, Image.SCALE_SMOOTH);
@@ -85,19 +85,34 @@ public class LoginPanel extends JFrame{
 		for(int i=0; i<6; i++) {
 			fields.add(new JTextField());
 		}
+		for(int i=0;i<6;i++){
+			Choice lista =new Choice();
+			lista.addItem("Aleatorio");
+			lista.addItem("Coche");
+			lista.addItem("Pelota");
+			lista.addItem("Sombrero");
+			lista.addItem("Esfinge");
+			this.choices.add(lista);
+		}
 		this.add(n1_label);
+		this.add(choices.get(0));
 		this.add(fields.get(0));
 		this.add(n2_label);
+		this.add(choices.get(1));
 		this.add(fields.get(1));
 		this.add(n3_label);
+		this.add(choices.get(2));
 		this.add(fields.get(2));
 		this.add(n4_label);
+		this.add(choices.get(3));
 		this.add(fields.get(3));
 		this.add(n5_label);
+		this.add(choices.get(4));
 		this.add(fields.get(4));
 		this.add(n6_label);
+		this.add(choices.get(5));
 		this.add(fields.get(5));
-		
+
 		comenzar = new JButton("Comenzar el juego");
 		comenzar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -107,13 +122,17 @@ public class LoginPanel extends JFrame{
 						nJugadores = Integer.parseInt(b.getText());
 					}
 				}
+				int isEqual = 0;
 				for(int i=0; i<nJugadores; i++) {
 				    String aux;
 				    if(fields.get(i).getText().length() >= 15)
 				        aux = fields.get(i).getText().substring(0,14);
-                    else
+                    else if(fields.get(i).getText().equals("")){
+							aux= "Jugador"+(i+1);
+					}
+				    else
                         aux = fields.get(i).getText();
-                    int isEqual = 0;
+
                     for(String s: nombres){
                         if(aux.equals(s))
                             isEqual++;
@@ -122,10 +141,11 @@ public class LoginPanel extends JFrame{
                         aux = aux + isEqual;
 
                     nombres.add(aux);
+					avatares.add(choices.get(i).getItem(choices.get(i).getSelectedIndex()));
 				}
 				((JButton) e.getSource()).getParent().setVisible(false);
 				try {
-					interfaz.cargarJugadoresIniciales(nombres,nombres,salida);
+					interfaz.cargarJugadoresIniciales(nombres,avatares,salida);
 					interfaz.cargarFichasIniciales();
 				} catch (IOException e1) {
 					e1.printStackTrace();
@@ -135,16 +155,22 @@ public class LoginPanel extends JFrame{
 		});
 		this.add(comenzar);
 	}
-	
+
 	public static ArrayList<String> getNombres(){
 		if(nombres.isEmpty()) {
 			return null;
 		}
 		return nombres;
 	}
-	
+
 	public static Integer getNJugadores() {
 		return nombres.size();
+	}
+	public static ArrayList<String> getAvatares(){
+		if(nombres.isEmpty()) {
+			return null;
+		}
+		return nombres;
 	}
 
 }
